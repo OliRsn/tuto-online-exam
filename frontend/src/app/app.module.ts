8a7ef937ab3f9b6bfdc143c76b7c46febef9d4f7
AROSIN <antoine.rosin@sia-partners.com>
2019-08-16 21:43:23 +0200

    add form + front authentification + front connection to Auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import * as Auth0 from 'auth0-web';
+import {CallbackComponent} from './callback.component';
+
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
@@ -5,16 +8,41 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {ExamsApiService} from './exams/exams-api.service';
 
+import {ExamFormComponent} from "./exams/exam-form.component";
+import {ExamsComponent} from "./exams/exams.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'callback', component: CallbackComponent },
+  { path: 'new-exam', component: ExamFormComponent },
+  { path: '', component: ExamsComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamFormComponent,
+    ExamsComponent,
+    CallbackComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor() {
+    Auth0.configure({
+      domain: 'dev-zp1m-nvi.eu.auth0.com',
+      audience: 'https://online-exam.digituz.com.fr',
+      clientID: '3v7UmmDWuQvk1zoHMfK1witmd5ZkydOZ',
+      redirectUri: 'http://localhost:4200/callback',
+      scope: 'openid profile manage:exams'
+    });
+  }
 }

a722b7102a76350185966b49d2488388783756da
AROSIN <antoine.rosin@sia-partners.com>
2019-08-18 22:22:03 +0200

    add route to onglet Online Exams

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,7 +5,7 @@ import * as Auth0 from 'auth0-web';
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary" class="mat-elevation-z10">
-      <button mat-button>Online Exams</button>
+      <button mat-button routerLink="/"> Online Exams</button>
       <button mat-button>About</button>
 
       <!-- This fills the remaining space of the current row -->
